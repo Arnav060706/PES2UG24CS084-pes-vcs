f73d336c42399deb9cdcc1477a1011ed23ef8c73
Arnav <arnavsharath@gmail.com>
2026-04-18 00:08:35 +0530

    Fixed commit.c file

--- commit.c
+++ commit.c
@@ -208,10 +208,11 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     } else {
         commit.has_parent = 0;
     }
+
     snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
     commit.timestamp = (uint64_t)time(NULL);
     snprintf(commit.message, sizeof(commit.message), "%s", message);
-    
+
     void *raw = NULL;
     size_t raw_len = 0;
     if (commit_serialize(&commit, &raw, &raw_len) != 0) return -1;
